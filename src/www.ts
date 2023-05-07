5012faea75e366f2f49e817567233a830c9604e1
Kehinde Fasunle <kfasunle@gmail.com>
2023-05-07 06:57:57 +0100

    feat: a simple server

---
+++ src/www.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const PORT = process.env.PORT || 5000;
+const server = express();
+const onServerStartup = () => `Server is running on http://localhost:${PORT}`;
+
+// middlewares
+
+// routes
+server.get("/", (req, res) =>
+  res.send({ server: "Miratonrose server is live 🚀" })
+);
+
+// listen to server
+server.listen(PORT, onServerStartup);
